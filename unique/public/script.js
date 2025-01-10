5d01d0783db2b9fc6c52b81cd8cf62a68b2c75a5
Amir Khan <94292231+imamirullah@users.noreply.github.com>
2025-01-10 11:40:48 +0530

    Update script.js

--- unique/public/script.js
+++ unique/public/script.js
@@ -32,7 +32,7 @@ document
 
     // Send data to the server
     try {
-      const response = await fetch("http://localhost:5000/generate", {
+      const response = await fetch("/generate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, phone }),
